728191994ec5d0360526352686c9e7263a3c95b4
某君more <moujunmore@qq.com>
2024-10-24 10:22:36 +0800

    fix(custom): 脚本升级脚本不生效

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
 output_file="upgrade.log"
 exec > >(tee -a "$output_file") 2>&1
 
-cp -rf web-firewalld/resource/public /usr/local/web-firewalld/resource/public
+rm -rf /usr/local/web-firewalld/resource/public
+cp -rf web-firewalld/resource/public /usr/local/web-firewalld/resource/
+rm -rf /usr/local/web-firewalld/web-firewalld
 cp -f web-firewalld/server /usr/local/web-firewalld/web-firewalld
 chmod +x  /usr/local/web-firewalld/web-firewalld
 
-cp -f web-firewalld.service /etc/systemd/system/
+cat  web-firewalld.service >  /etc/systemd/system/web-firewalld.service
 
 systemctl daemon-reload
 systemctl restart web-firewalld
